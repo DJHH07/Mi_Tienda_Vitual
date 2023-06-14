ccd197812484eccc3ffe5e0b5c6f30ebd377bcba
hosDan1.01 <djhh03@gmail.com>
2023-06-13 19:59:47 -0600

    h

--- JS/Tienda.js
+++ JS/Tienda.js
@@ -177,7 +177,6 @@ productList.addEventListener('click', e => {
 
 
 
-
 //Aca hicimos todo lo de la suma del carrito
 //evento que eliminara los productos que no deseamos en el carrito
 rowProduct.addEventListener('click', e =>{
